e68d57ee1c8c32ae29c442c1de1ec97b57971641
Troy Madsen <madsentr@eos17.cis.gvsu.edu>
2017-03-23 15:06:17 -0400

    -Finalized python script to run simulations

--- Run.py
+++ Run.py
@@ -2,11 +2,15 @@
 
 import os
 
-# Tuples to be run. Each is of the form [duration, model].
-ops = [[100, 3],
-       [200, 3],
-       [400, 3]]
+# Tuples to be run. Each is of the form [[Models], [Traffic Densities],
+# [Execution durations], [Speed Limits]].
+ops = [[1, 2, 3],
+       [100, 200, 400, 800],
+       [100, 500, 1500, 2500],
+       [15, 25, 35]]
 
-for o in ops:
-    for r in range(20):
-        os.system('java -ea -server -Xmx1000M -jar aim4.jar -headless -e ' + str(o[0]) + ' -r ' + str(r) + ' -m ' + str(o[1]) + ' -f "Model' + str(o[1]) + '.log"')
+for m in ops[0]:
+    for e in ops[1]:
+        for d in ops[2]:
+            for s in ops[3]:
+                os.system('java -ea -server -Xmx1000M -jar aim4-root/aim4.jar -headless -m ' + str(m) + ' -e ' + str(e) + ' -d ' + str(d) + ' -s ' + str(s) + ' -f "Model' + str(m) + '.log"')
