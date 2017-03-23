72b06b1dffd64fb5ee6477204b709f253ea91043
Troy Madsen <troylmadsen@gmail.com>
2017-03-23 12:16:52 -0400

    -Created first attempted at multiple logging runs

--- Run.py
+++ Run.py
@@ -2,4 +2,11 @@
 
 import os
 
-os.system('java -ea -server -Xmx1000M -jar aim4.jar -headless -e 300 -r 1 -m 3 -f "TestRun1"')
+# Tuples to be run. Each is of the form [duration, model].
+ops = [[100, 3],
+       [200, 3],
+       [400, 3]]
+
+for o in ops:
+    for r in range(20):
+        os.system('java -ea -server -Xmx1000M -jar aim4.jar -headless -e ' + str(o[0]) + ' -r ' + str(r) + ' -m ' + str(o[1]) + ' -f "Model' + str(o[1]) + '.log"')
