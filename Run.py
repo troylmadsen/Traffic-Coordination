b9953cbbdcae03f66eb8299ff9b6e6228ab8373d
Troy Madsen <madsentr@eos17.cis.gvsu.edu>
2017-12-08 04:37:25 -0500

    -New Run file for thorough testing

--- Run.py
+++ Run.py
@@ -2,19 +2,28 @@
 
 import os
 
-# Removing old log files
-os.system('rm Model?.log')
+# Removing old log files 0 - 999
+os.system('rm Model*.log')
 
 # Tuples to be run. Each is of the form [[Models], [Traffic Densities],
 # [Execution durations], [Speed Limits]].
-ops = [[1, 2, 3, 4],
+ops = [[4],
        [100, 200, 400, 800],
        [100, 500, 1500, 2500],
        [15, 25, 35]]
 
-for m in ops[0]:
-    for e in ops[1]:
-        for d in ops[2]:
-            for s in ops[3]:
-                for r in range(20):
-                    os.system('java -ea -server -Xmx1000M -jar aim4-root/aim4.jar -headless -r ' + str(r) + ' -m ' + str(m) + ' -e ' + str(e) + ' -d ' + str(d) + ' -s ' + str(s) + ' -f "Model' + str(m) + '.log"' + ' -U 0 -D 10 -N 0 -M 9999 -Z 0 -X 9999 -L 0 -H 9999')
+# Limits of the speed adjustment curves
+# [[Means], [Standard Deviations], [[maxRed, minRed, minInc, maxInc]]]
+limits = [[0],
+          [8, 10, 12],
+          [[15, 5, 5, 15], [15, 0, 0, 15], [20, 0, 0, 20]]]
+
+for mean in limits[0]:
+    for std in limits[1]:
+        for lims in limits[2]:
+            for m in ops[0]:
+                for e in ops[1]:
+                    for d in ops[2]:
+                        for s in ops[3]:
+                            for r in range(20):
+                                os.system('java -ea -server -Xmx1000M -jar aim4-root/aim4.jar -headless -r ' + str(r) + ' -m ' + str(m) + ' -e ' + str(e) + ' -d ' + str(d) + ' -s ' + str(s) + ' -f "Model_' + str(m) + '_' + str(mean) + '_' + str(std) + '.log"' + ' -U ' + str(mean) + ' -D ' + str(std) + ' -M ' + str(lims[0]) + ' -N ' + str(lims[1]) + ' -Z ' + str(lims[2]) + ' -X ' + str(lims[3]) + ' -L 0 -H 9999')
